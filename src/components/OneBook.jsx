de005585e8585aef586872ce0a7b4e76a8e1360f
slavaPrus <slava.ei@gmail.com>
2024-07-03 19:52:14 +0300

    fix click in one book

--- src/components/OneBook.jsx
+++ src/components/OneBook.jsx
@@ -1,27 +1,40 @@
-
 import { Box, Button, Typography } from "@mui/material";
-import React from "react";
-import bamidbar from "../pictures/bamidbar.png";
+import React, { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import BookAction from "./BookAction";
-import ImageView from "./ImageView"; 
+import ImageView from "./ImageView";
 
 export default function OneBook() {
   const location = useLocation();
-  const book = location.state;
-  const { bookName, cost, describe, img, bookUrl } = book;
+  const [book, setBook] = React.useState(location.state);
+
+  useEffect(() => {
+    if (location.state) {
+      // If there's book data in the location state, save it to sessionStorage
+      sessionStorage.setItem("book", JSON.stringify(location.state));
+    } else {
+      // If there's no book data in the location state, try to get it from sessionStorage
+      const savedBook = sessionStorage.getItem("book");
+      if (savedBook) {
+        setBook(JSON.parse(savedBook));
+      }
+    }
+  }, [location.state]);
 
   console.log("Received book:", book); // Debugging line
 
   // Ensure book object exists and has the expected properties
-  if (!book ) {
+  if (!book) {
     return <Typography>Invalid book data</Typography>;
   }
 
-  // const { bookName, cost, describe, img, bookUrl } = book;
-  
+  const { bookName, cost, describe, img, bookUrl } = book;
+
   const handleWatchBook = () => {
-    window.open("https://firebasestorage.googleapis.com/v0/b/tifertshlomofirebase.appspot.com/o/2Halachas%2F%D7%A9%D7%AA%D7%99%20%D7%94%D7%9C%D7%9B%D7%95%D7%AA%20%D7%AA%D7%A9%D7%A4%D7%93%20%D7%A0%D7%A9%D7%9C%D7%97%20%D7%9C%D7%93%D7%A4%D7%95%D7%A1%20(1).pdf?alt=media&token=a30af0eb-2055-4696-a95a-b6d7e92d4b6d", "_blank");
+    window.open(
+      "https://firebasestorage.googleapis.com/v0/b/tifertshlomofirebase.appspot.com/o/2Halachas%2F%D7%A9%D7%AA%D7%99%20%D7%94%D7%9C%D7%9B%D7%95%D7%AA%20%D7%AA%D7%A9%D7%A4%D7%93%20%D7%A0%D7%A9%D7%9C%D7%97%20%D7%9C%D7%93%D7%A4%D7%95%D7%A1%20(1).pdf?alt=media&token=a30af0eb-2055-4696-a95a-b6d7e92d4b6d",
+      "_blank"
+    );
   };
 
   return (
@@ -31,6 +44,7 @@ export default function OneBook() {
       alignItems={"center"}
       textAlign={"center"}
       gap={"30px"}
+      paddingY={"55px"}
     >
       <Box
         maxWidth={"250px"}
@@ -57,14 +71,13 @@ export default function OneBook() {
         <Typography
           sx={{ fontSize: "25px", fontWeight: "800", textAlign: "center" }}
         >
-         ₪ {cost} 
+          ₪ {cost}
         </Typography>
         {/* <BookAction book={book} setAlert={setAlert}/> */}
       </Box>
-      <Box width={"30%"} height={"70%"} overflow={"auto"}>
-      <ImageView imageUrl={bookUrl } height={"600px"}></ImageView>
-       </Box>
+      <Box width={"50%"} overflow={"auto"}>
+        <ImageView imageUrl={bookUrl}></ImageView>
+      </Box>
     </Box>
-
   );
 }
